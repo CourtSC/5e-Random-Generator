1019ebbdee1155c97fe1fdc12468677081123675
CourtSC <scott.c.court@gmail.com>
2021-11-25 11:12:51 -0500

    Cleaned up code.

--- Magic Item Tables/ddbData.py
+++ Magic Item Tables/ddbData.py
@@ -2,29 +2,10 @@
 #! dataPull.py - A program to scrape D&D 5e magic item info from various sources and save them to a database
 #! Edge User-Agent - Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29
 
-import bs4, requests_html, sqlite3, csv
+import bs4, requests_html, csv
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29'}
 session = requests_html.HTMLSession()
-
-# Create database. Commented out until I can learn SQLite.
-'''con = sqlite3.connect('Magic Item Tables/magicItem.db')
-sql = 'INSERT INTO MAGICITEMS (name, rarity, type, subtype, attune, notes) values(?, ?, ?, ?, ?, ?)'
-try:
-    with con:
-        con.execute("""
-            CREATE TABLE MAGICITEMS (
-                name TEXT,
-                rarity TEXT,
-                type TEXT,
-                subtype TEXT,
-                attune TEXT,
-                notes TEXT
-            );
-        """)
-except:
-    pass
-'''
 
 data = []
 page = 1
@@ -48,34 +29,9 @@ while nextPage:
             data.append((itemName[i].getText().strip(), itemRarity[i].getText().strip(), itemType[i].getText().strip(), itemSubType[i].getText().strip(), itemAttunement[i].getText().strip(), itemNotes[i].getText().strip()))
         else:
             data.append((itemName[i].getText().strip(), itemRarity[i].getText().strip(), itemType[i].getText().strip(), itemSubType[i].getText().strip(), 'Not Required', itemNotes[i].getText().strip()))
-        # print(f'Added item {itemName[i].getText().strip()} to database.')
-
-'''def itemCheck(item):
-    with open('Magic Item Tables/MagicItems.csv', 'r') as dBase:
-        reader = csv.reader(dBase, delimiter = '\n')
-        for row in reader:
-            if item in row:
-                return True
-        return False
-'''
 
 with open('Magic Item Tables/MagicItems.csv', 'w') as dBase:
     writer = csv.writer(dBase)
     for i in data:
-        # if not itemCheck(i[0]):
         writer.writerow(i)
-
-# Add new data to the MAGICITEMS table in the database.
-'''def itemCheck(item):
-    with con:
-        dbItem = con.execute('SELECT * FROM MAGICITEMS')
-        if item in dbItem:
-            return True
-        else:
-            return False
-
-with con:
-    for i in range(len(itemName)):
-        if not itemCheck(itemName[i].getText().strip()):
-            con.execute(sql, data[i])
-'''
+        print(f'Added item {i[0]} to database.')
