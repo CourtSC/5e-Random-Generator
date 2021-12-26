2a74c617f8e0297f99d654be5cc1d7f29ffe994b
CourtSC <scott.c.court@gmail.com>
2021-12-26 16:01:58 -0500

    Commented out gold, art, and gem rewards to simplify returned value.

--- Magic Item Tables/rollTables.py
+++ Magic Item Tables/rollTables.py
@@ -13,19 +13,19 @@ def magicTableCR4(d100):
     conTable = []
     itemTable = []
     rareTable = []
-    gems = sum([randint(1,6) for i in range(2)])
-    art = sum([randint(1,4) for i in range(2)])
-    cp = sum([randint(1,6) for i in range(6)])
-    sp = sum([randint(1,6) for i in range(3)])
-    gp = sum([randint(1,6) for i in range(2)])
-    reward.append((f'{cp * 100} CP', f'{sp * 100} SP', f'{gp * 10} GP'))
-    # Generate art or gem rewards 
-    if d100 in [i for i in range(7, 17)] + [i for i in range(37, 45)] + [i for i in range(61, 66)] + [i for i in range(76, 79)]:
-        reward.append(f'{gems * 10} gp worth of gems.')
-    elif d100 in [i for i in range(17, 27)] + [i for i in range(45, 53)] + [i for i in range(66, 71)] + [i for i in range(79, 81)] + [i for i in range(86, 93)] + [i for i in range(98, 100)]:
-        reward.append(f'{art * 25} gp worth of art objects.')
-    elif d100 in [i for i in range(27,37)] + [i for i in range(53,61)] + [i for i in range(71,76)] + [i for i in range(81,86)] + [i for i in range(93,98)] + [i for i in range(100, 101)]:
-        reward.append(f'{gems * 50} gp worth of gems.')
+    # gems = sum([randint(1,6) for i in range(2)])
+    # art = sum([randint(1,4) for i in range(2)])
+    # cp = sum([randint(1,6) for i in range(6)])
+    # sp = sum([randint(1,6) for i in range(3)])
+    # gp = sum([randint(1,6) for i in range(2)])
+    # reward.append((f'{cp * 100} CP', f'{sp * 100} SP', f'{gp * 10} GP'))
+    # # Generate art or gem rewards 
+    # if d100 in [i for i in range(7, 17)] + [i for i in range(37, 45)] + [i for i in range(61, 66)] + [i for i in range(76, 79)]:
+    #     reward.append(f'{gems * 10} gp worth of gems.')
+    # elif d100 in [i for i in range(17, 27)] + [i for i in range(45, 53)] + [i for i in range(66, 71)] + [i for i in range(79, 81)] + [i for i in range(86, 93)] + [i for i in range(98, 100)]:
+    #     reward.append(f'{art * 25} gp worth of art objects.')
+    # elif d100 in [i for i in range(27,37)] + [i for i in range(53,61)] + [i for i in range(71,76)] + [i for i in range(81,86)] + [i for i in range(93,98)] + [i for i in range(100, 101)]:
+    #     reward.append(f'{gems * 50} gp worth of gems.')
     # Generate list of Consumables.
     with open(Path.home() / Path('5e-Random-Generator/Magic Item Tables/MagicItems.csv'), 'r', encoding='Windows-1252') as dBase:
         reader = csv.reader(dBase, delimiter = '\n')
@@ -86,21 +86,21 @@ def magicTableCR10(d100):
     conTable = []
     itemTable = []
     itemTableLow = []
-    gems = sum([randint(1,6) for i in range(3)])
-    art = sum([randint(1,4) for i in range(2)])
-    cp = sum([randint(1,6) for i in range(2)])
-    sp = sum([randint(1,6) for i in range(2)])
-    gp = sum([randint(1,6) for i in range(6)])
-    pp = sum([randint(1,6) for i in range(3)])
-    reward.append((f'{cp * 100} CP', f'{sp * 1000} SP', f'{gp * 10} GP', f'{pp * 10} PP'))
-    if d100 in [i for i in range(5,11)] + [i for i in range(29,33)] + [i for i in range(45,50)] + [i for i in range(64,66)] + [i for i in range(75,77)] + [i for i in range(81,85)]:
-        reward.append(f'{art * 25} gp worth of art objects.')
-    elif d100 in [i for i in range(11,17)] + [i for i in range(33,37)] + [i for i in range(50,55)] + [i for i in range(67,70)] + [i for i in range(77,79)] + [i for i in range(85,89)]:
-        reward.append(f'{gems * 50} gp worth of gems.')
-    elif d100 in [i for i in range(17,23)] + [i for i in range(37,41)] + [i for i in range(55,60)] + [i for i in range(70,73)] + [79] + [i for i in range(89,92)] + [i for i in range(95,97)] + [99]:
-        reward.append(f'{gems * 100} worth of gems.')
-    elif d100 in [i for i in range(23,29)] + [i for i in range(41,45)] + [i for i in range(60,64)] + [i for i in range(73,75)] + [80] + [i for i in range(92,95)] + [i for i in range(97,99)] + [100]:
-        reward.append(f'{art * 250} worth of art objects.')
+    # gems = sum([randint(1,6) for i in range(3)])
+    # art = sum([randint(1,4) for i in range(2)])
+    # cp = sum([randint(1,6) for i in range(2)])
+    # sp = sum([randint(1,6) for i in range(2)])
+    # gp = sum([randint(1,6) for i in range(6)])
+    # pp = sum([randint(1,6) for i in range(3)])
+    # reward.append((f'{cp * 100} CP', f'{sp * 1000} SP', f'{gp * 10} GP', f'{pp * 10} PP'))
+    # if d100 in [i for i in range(5,11)] + [i for i in range(29,33)] + [i for i in range(45,50)] + [i for i in range(64,66)] + [i for i in range(75,77)] + [i for i in range(81,85)]:
+    #     reward.append(f'{art * 25} gp worth of art objects.')
+    # elif d100 in [i for i in range(11,17)] + [i for i in range(33,37)] + [i for i in range(50,55)] + [i for i in range(67,70)] + [i for i in range(77,79)] + [i for i in range(85,89)]:
+    #     reward.append(f'{gems * 50} gp worth of gems.')
+    # elif d100 in [i for i in range(17,23)] + [i for i in range(37,41)] + [i for i in range(55,60)] + [i for i in range(70,73)] + [79] + [i for i in range(89,92)] + [i for i in range(95,97)] + [99]:
+    #     reward.append(f'{gems * 100} worth of gems.')
+    # elif d100 in [i for i in range(23,29)] + [i for i in range(41,45)] + [i for i in range(60,64)] + [i for i in range(73,75)] + [80] + [i for i in range(92,95)] + [i for i in range(97,99)] + [100]:
+    #     reward.append(f'{art * 250} worth of art objects.')
     # Generate list of Consumables.
     with open(Path.home() / Path('5e-Random-Generator/Magic Item Tables/MagicItems.csv'), 'r', encoding='Windows-1252') as dBase:
         reader = csv.reader(dBase, delimiter = '\n')
@@ -182,19 +182,19 @@ def magicTableCR16(d100):
     conTable = []
     itemTable = []
     itemTableLow = []
-    gems = sum([randint(1,6) for i in range(3)])
-    art = sum([randint(1,4) for i in range(2)])
-    gp = sum([randint(1,6) for i in range(4)])
-    pp =  sum([randint(1,6) for i in range(5)])
-    reward.append((f'{gp * 10} GP', f'{pp * 10} PP'))
-    if d100 in [i for i in range(4,7)] + [i for i in range(16,20)] + [i for i in range(30,36)] + [i for i in range(51,55)] + [i for i in range(67,69)] + [i for i in range(75,77)] + [i for i in range(83,85)] + [i for i in range(93,95)]:
-        reward.append(f'{art * 250} gp worth of art objects.')
-    elif d100 in [i for i in range(7,10)] + [i for i in range(20,24)] + [i for i in range(36,41)] + [i for i in range(55,59)] + [i for i in range(69,71)] + [i for i in range(77,79)] + [i for i in range(86,89)] + [i for i in range(95,97)]:
-        reward.append(f'{art * 750} gp worth of art objects.')
-    elif d100 in [i for i in range(10,13)] + [i for i in range(24,27)] + [i for i in range(41,46)] + [i for i in range(59,63)] + [i for i in range(71,73)] + [i for i in range(79,81)] + [i for i in range(97,99)]:
-        reward.append(f'{gems * 500} gp worth of gems.')
-    elif d100 in [i for i in range(13,16)] + [i for i in range(27,30)] + [i for i in range(46,51)] + [i for i in range(63,67)] + [i for i in range(73,75)] + [i for i in range(81,83)] + [i for i in range(91,93)] + [i for i in range(99,101)]:
-        reward.append(f'{gems * 1000} gp worth of gems.')
+    # gems = sum([randint(1,6) for i in range(3)])
+    # art = sum([randint(1,4) for i in range(2)])
+    # gp = sum([randint(1,6) for i in range(4)])
+    # pp =  sum([randint(1,6) for i in range(5)])
+    # reward.append((f'{gp * 10} GP', f'{pp * 10} PP'))
+    # if d100 in [i for i in range(4,7)] + [i for i in range(16,20)] + [i for i in range(30,36)] + [i for i in range(51,55)] + [i for i in range(67,69)] + [i for i in range(75,77)] + [i for i in range(83,85)] + [i for i in range(93,95)]:
+    #     reward.append(f'{art * 250} gp worth of art objects.')
+    # elif d100 in [i for i in range(7,10)] + [i for i in range(20,24)] + [i for i in range(36,41)] + [i for i in range(55,59)] + [i for i in range(69,71)] + [i for i in range(77,79)] + [i for i in range(86,89)] + [i for i in range(95,97)]:
+    #     reward.append(f'{art * 750} gp worth of art objects.')
+    # elif d100 in [i for i in range(10,13)] + [i for i in range(24,27)] + [i for i in range(41,46)] + [i for i in range(59,63)] + [i for i in range(71,73)] + [i for i in range(79,81)] + [i for i in range(97,99)]:
+    #     reward.append(f'{gems * 500} gp worth of gems.')
+    # elif d100 in [i for i in range(13,16)] + [i for i in range(27,30)] + [i for i in range(46,51)] + [i for i in range(63,67)] + [i for i in range(73,75)] + [i for i in range(81,83)] + [i for i in range(91,93)] + [i for i in range(99,101)]:
+    #     reward.append(f'{gems * 1000} gp worth of gems.')
     # Generate list of Consumables.
     with open(Path.home() / Path('5e-Random-Generator/Magic Item Tables/MagicItems.csv'), 'r', encoding='Windows-1252') as dBase:
         reader = csv.reader(dBase, delimiter = '\n')
@@ -321,19 +321,19 @@ def magicTableCR17(d100):
     conTable = []
     itemTable = []
     itemTableLow = []
-    gems = sum([randint(1,6) for i in range(3)])
-    art = sum([randint(1,4) for i in range(2)])
-    gp = sum([randint(1,6) for i in range(12)])
-    pp =  + sum([randint(1,6) for i in range(8)])
-    reward.append((f'{gp * 10} GP', f'{pp * 10} PP'))
-    if d100 in [i for i in range(3,6)] + [i for i in range(15,23)] + [i for i in range(47,53)] + [69] + [i for i in range(73,75)] + [i for i in range(81,86)]:
-        reward.append(f'{gems * 1000} gp worth of gems.')
-    elif d100 in [i for i in range(6,9)] + [i for i in range(23,31)] + [i for i in range(53,59)] + [70] + [i for i in range(75,77)] + [i for i in range(86,91)]:
-        reward.append(f'{art * 2500} gp worth of art.')
-    elif d100 in [i for i in range(9,12)] + [i for i in range(31,39)] + [i for i in range(59,64)] + [71]  + [i for i in range(77,79)] + [i for i in range(91,96)]:
-        reward.append(f'{art * 7500} gp worth of art.')
-    elif d100 in [i for i in range(12,15)] + [i for i in range(39,47)] + [i for i in range(64,69)] + [72] + [i for i in range(79,81)] + [i for i in range(96,101)]:
-        reward.append(f'{gems * 5000} gp worth of gems.')
+    # gems = sum([randint(1,6) for i in range(3)])
+    # art = sum([randint(1,4) for i in range(2)])
+    # gp = sum([randint(1,6) for i in range(12)])
+    # pp =  + sum([randint(1,6) for i in range(8)])
+    # reward.append((f'{gp * 10} GP', f'{pp * 10} PP'))
+    # if d100 in [i for i in range(3,6)] + [i for i in range(15,23)] + [i for i in range(47,53)] + [69] + [i for i in range(73,75)] + [i for i in range(81,86)]:
+    #     reward.append(f'{gems * 1000} gp worth of gems.')
+    # elif d100 in [i for i in range(6,9)] + [i for i in range(23,31)] + [i for i in range(53,59)] + [70] + [i for i in range(75,77)] + [i for i in range(86,91)]:
+    #     reward.append(f'{art * 2500} gp worth of art.')
+    # elif d100 in [i for i in range(9,12)] + [i for i in range(31,39)] + [i for i in range(59,64)] + [71]  + [i for i in range(77,79)] + [i for i in range(91,96)]:
+    #     reward.append(f'{art * 7500} gp worth of art.')
+    # elif d100 in [i for i in range(12,15)] + [i for i in range(39,47)] + [i for i in range(64,69)] + [72] + [i for i in range(79,81)] + [i for i in range(96,101)]:
+    #     reward.append(f'{gems * 5000} gp worth of gems.')
     # Generate list of Consumables.
     with open(Path.home() / Path('5e-Random-Generator/Magic Item Tables/MagicItems.csv'), 'r', encoding='Windows-1252') as dBase:
         reader = csv.reader(dBase, delimiter = '\n')
